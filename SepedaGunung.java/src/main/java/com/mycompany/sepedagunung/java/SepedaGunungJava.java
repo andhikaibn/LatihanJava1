dab71c30b18647e2046db9b983e1421e41cdc42e
andhikaibn <astroid2200@gmail.com>
2024-03-28 02:44:33 +0700

    nambah hierarkioperator

---
+++ SepedaGunung.java/src/main/java/com/mycompany/sepedagunung/java/SepedaGunungJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pewarisan;
+
+/**
+ *
+ * @author ANDIKA
+ */
+class SepedaGunung extends Sepeda {
+int setSadel;
+public void setSadel(int nilaiSadel) (
+setSadel = nilaiSadel;
+
+System.out.prinlt("Tinggi Sadel:"+setSadel);
+)
+}
