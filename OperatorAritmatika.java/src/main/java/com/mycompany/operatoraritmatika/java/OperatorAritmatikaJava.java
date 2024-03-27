1490ae1fc9eda55a84d14829890d9411093a50ce
andhikaibn <astroid2200@gmail.com>
2024-03-28 05:34:50 +0700

    tambah op op

---
+++ OperatorAritmatika.java/src/main/java/com/mycompany/operatoraritmatika/java/OperatorAritmatikaJava.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author ANDIKA
+ */
+public class OperatorAritmatika {
+
+public static void main(String[] args) {
+//Deklarasi dan inisiasi variable 
+int i = 9 ;
+int j = 4 ;
+//Menampilkan Nilai Variable
+System.out.println ( " Nilai variabel : " );
+System.out.println ( " i = " + i );
+System.out.println ( " j = " + j );
+//Menggunakan operator penjumlahan 
+System.out.println ( " Penjumlahan : ") ;
+System.out.println ( " i + j = " + (i + j));
+//Menggunakan operator pengurangan 
+System.out.println ( " Pengurangan : ") ;
+System.out.println ( " i - j = " + (i - j));
+//Menggunakan operator perkalian
+System.out.println ( " Perkalian : ") ;
+System.out.println ( " i * j = " + (i * j));
+//Menggunakan operator pembagian
+System.out.println ( " Pembagian : ") ;
+System.out.println ( " i / j = " + (i / j));
+//Menggunakan operator modulus
+System.out.println ( " Modulus : ") ;
+System.out.println ( " i mod j = " + (i % j));
+    }
+}
