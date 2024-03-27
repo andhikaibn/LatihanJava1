a8e78aa98d64e360e2e1e4b0e5977b1afacbbfed
andhikaibn <astroid2200@gmail.com>
2024-03-28 05:51:47 +0700

    belajar java

---
+++ Pintar/src/main/java/com/mycompany/pintar/Pintar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pintar.java;
+
+/**
+ *
+ * @author ANDIKA
+ */
+public class Pintar {
+public static void main(String[] args) {
+Belajar panggil = nev Belajar();
+System.out.println("Panggil X : "+panggil.x);
+System.out.println("Panggil Y : "+panggil.y);
+    }
+}
