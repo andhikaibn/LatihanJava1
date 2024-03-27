ae4d47150d6499e0f94439e2d233855f2cabb0e1
andhikaibn <astroid2200@gmail.com>
2024-03-28 02:26:45 +0700

    sepeda java

---
+++ Sepeda.java/src/main/java/com/mycompany/sepeda/java/SepedaJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pewarisan;
+
+/**
+ *
+ * @author ANDIKA
+ */
+class Sepeda {
+int kecepatan = 0;
+int gir = 0;
+//method
+void ubahGir(int pertambahanGir) (
+gir = gir + pertambahanGir
+System.out.println("Gir:" + gir) 
+);
+
+void tambahKecepatan (int pertambahanKecepatan)(
+kecepatan = kecepatan + pertambahanKecepatan;
+System.out.println("kecepatan" + kecepatan);
+)
+}
