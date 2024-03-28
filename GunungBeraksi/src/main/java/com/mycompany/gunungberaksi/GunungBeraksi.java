a5bf9536cde1be979990d19c89929d5e4f2da151
andhikaibn <astroid2200@gmail.com>
2024-03-28 12:40:47 +0700

    tambah sepedagunng beraksi dll

---
+++ GunungBeraksi/src/main/java/com/mycompany/gunungberaksi/GunungBeraksi.java
@@ -0,0 +1,17 @@
+
+package pewarisan;  
+
+/**
+ *
+ * @author ANDIKA
+ */
+public class SepedaGunungBeraksi {
+    public static void main(String[] args) {
+// Membuat object
+SepedaGunung sepedaku = new SepedaGunung();
+//memanggil method di object
+sepedaku.tambahKecepatan(10);
+sepedaku.ubahGir(2);
+sepedaku.setSadel(20);
+    }
+}
---
+++ OptPenambahPengurang.java/src/main/java/com/mycompany/optpenambahpengurang/java/OptPenambahPengurangJava.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.optpenambahpengurang.java;
+
+/**
+ *
+ * @author ANDIKA
+ */
+public class OptPenambahPenguran {
+    public static void main(String[] args) {
+        int a, b;
+        a = 7;
+        b = 3;
+        
+        System.out.println("Nilai A = " + a);
+        System.out.println("Nilai ++A = " + ++a);
+        System.out.println("Nilai A++ = " + a++);
+        System.out.println("Nilai A = " + a);
+        System.out.println("--------------");
+        System.out.println("Nilai B = " + b);
+        System.out.println("Nilai ++B = " + ++b);
+        System.out.println("Nilai B++ = " + b++);
+        System.out.println("Nilai B = " + b);
+    }
+    
+}
